971834275542ebc95b56ce7bc617f299d10daa4e
Varma, Naveen <nvarma@contractor.jcp.com>
2016-11-11 09:53:21 -0600

    CODE FOR WORKING WEATHER APP

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+import BooksReducer from './reducer_books';
+import ActiveBook from './reducer_active_book.js';
+import WeatherReducer from './reducer_weather.js';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  weather: WeatherReducer
 });
 
 export default rootReducer;
